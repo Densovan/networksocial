7fb603b610bed52bc25546671efc9657ac75ed37
Densovan <sarimramata@gmail.com>
2021-08-28 22:00:06 +0700

    add client

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const connectDB = require("./configs/db");
+const color = require("colors");
 
 const app = express();
 
@@ -6,6 +8,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+//=================>ConnectDB<===============
+connectDB();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server Runnig on port: ${port}`));
